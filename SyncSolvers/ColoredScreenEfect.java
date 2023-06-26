f423047509138631094eb0abafb4da756b7cef06
BRYANAARONEDO2 <a266902@alumnos.uaslp.mx>
2023-06-26 16:33:13 -0600

    a

--- SyncSolvers/ColoredScreenEfect.java
+++ SyncSolvers/ColoredScreenEfect.java
@@ -8,12 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ColoredScreenEfect extends ScreenEffect
 {
-    /**
-     * Act - do whatever the ColoredScreenEfect wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        // Add your action code here.
-    }
+    
+    
 }
